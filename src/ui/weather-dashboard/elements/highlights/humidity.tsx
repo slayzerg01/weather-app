aac9d8efa61dcfcb9239029f8adb62d0b481c44b
Sarbassov <bagdat250716@gmail.com>
2024-03-24 12:52:21 +0500

    добавлена карточка инфой по влажности

--- src/ui/weather-dashboard/elements/highlights/humidity.tsx
+++ src/ui/weather-dashboard/elements/highlights/humidity.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
 
 export async function HumidityBlock (
     { humidity }: 
@@ -24,5 +25,33 @@ export async function HumidityBlock (
                 </div>  
             </div>
         </>
+    )
+}
+
+export function MobileHumidityBlock (
+    { humidity }: 
+    { 
+        humidity: number
+    }
+) {
+    return (
+        <Card className='dark:bg-zinc-800'>
+            <CardHeader className="pb-2">Humidity</CardHeader>
+            <CardBody className='flex p-2 flex-row justify-around items-center'>
+                <div className="flex flex-row flex-grow justify-around">
+                    <div className="flex flex-col justify-center">
+                        <span className="ml-2 text-gray-500 text-bold text-3xl mb-2">{humidity}<sup className="text-xl">%</sup></span>
+                        <span className="ml-2 text-gray-500 text-bold text-xl"> 
+                            { (30<=humidity)&&(humidity<=65) ? 'Normal' : humidity<= 35 ? 'Dry' : 'Wet'}
+                        </span>
+                    </div> 
+                    <div className="flex flex-row">
+                        <div className="flex flex-col flex-nowrap justify-end w-3 h-20 bg-gray-300 rounded-full overflow-hidden" role="progressbar" aria-valuenow={10} aria-valuemin={0} aria-valuemax={100}>
+                            <div className="rounded-full overflow-hidden bg-blue-600" style={{height: `${humidity}%`}}></div>
+                        </div>
+                    </div>  
+                </div>
+            </CardBody>
+        </Card>   
     )
 }
--- src/ui/weather-dashboard/modules/mobile-hightlights.tsx
+++ src/ui/weather-dashboard/modules/mobile-hightlights.tsx
@@ -1,9 +1,10 @@
 import { MobileUvIndex } from "../elements/highlights/uv-index";
 import { MobileWindBlock } from "../elements/highlights/wind-block";
 import { MobileSunriseSunsetBlock } from "../elements/highlights/sunrise-sunset";
+import { MobileHumidityBlock } from "../elements/highlights/humidity";
 import { Suspense } from "react";
 import { currentDayInfo, daysInfo } from "@/lib/definitions";
-import { Tab, Tabs, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
 
 export function TodaysHighlights(
     {days, current}: {days: daysInfo, current: currentDayInfo}
@@ -17,10 +18,7 @@ export function TodaysHighlights(
                     <MobileWindBlock windDirection={current.windDirection} windSpeed={current.windSpeed}/>
                 </Suspense>
                 <MobileSunriseSunsetBlock sunrise={days.sunrise[0]} sunset={days.sunset[0]}/>
-            <Card>
-                <CardHeader>Chel</CardHeader>
-                <CardBody>csdcsdc</CardBody>
-            </Card>
+                <MobileHumidityBlock humidity={current.relativeHumidity}/>
             <Card>
                 <CardHeader>Chel</CardHeader>
                 <CardBody>csdcsdc</CardBody>
